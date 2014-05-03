050e9567415f76f0b7b15b3cd528946f5feb98d8
Colin Walters <walters@verbum.org>
2014-05-03 13:33:12 -0400

    qa-make-disk: Enhance to allow directly creating images with content
    I'm trying to split up this code base, and there's a really good use
    case for being able to just directly make a qcow2 from a commit
    without running the autobuilder.

--- src/autobuilder/js/builtins/qa_make_disk.js
+++ src/autobuilder/js/builtins/qa_make_disk.js
@@ -22,6 +22,7 @@ const Lang = imports.lang;
 const Format = imports.format;
 
 const GSystem = imports.gi.GSystem;
+const OSTree = imports.gi.OSTree;
 
 const Builtin = imports.builtin;
 const ArgParse = imports.argparse;
@@ -37,17 +38,41 @@ const QaMakeDisk = new Lang.Class({
 
     _init: function() {
         this.parent();
+        this.parser.addArgument('repo');
+        this.parser.addArgument('osname');
+        this.parser.addArgument('ref');
         this.parser.addArgument('diskpath');
     },
 
     execute: function(args, loop, cancellable) {
+        let repoPath = Gio.File.new_for_path(args.repo);
+        let repo = new OSTree.Repo({ path: repoPath });
+        let [,rev] = repo.resolve_rev(args.ref, false);
         let path = Gio.File.new_for_path(args.diskpath);
         if (path.query_exists(null))
             throw new Error("" + path.get_path() + " exists");
-
         let tmppath = path.get_parent().get_child(path.get_basename() + '.tmp');
         GSystem.shutil_rm_rf(tmppath, cancellable);
         LibQA.createDisk(tmppath, cancellable);
+        let mntdir = Gio.File.new_for_path('mnt');
+        GSystem.file_ensure_directory(mntdir, true, cancellable);
+        let gfmnt = new GuestFish.GuestMount(tmppath, { partitionOpts: LibQA.DEFAULT_GF_PARTITION_OPTS,
+                                                            readWrite: true });
+        gfmnt.mount(mntdir, cancellable);
+        try {
+            let osname = args['osname'];
+            LibQA.pullDeploy(mntdir, repoPath, osname, args.ref, rev, null,
+                             cancellable, { addKernelArgs: [] });
+            print("Doing initial labeling");
+            ProcUtil.runSync(['ostree', 'admin', '--sysroot=' + mntdir.get_path(),
+                              'instutil', 'selinux-ensure-labeled',
+		                          mntdir.get_path(),
+		                          ""],
+		                         cancellable,
+		                         { logInitiation: true });
+        } finally {
+            gfmnt.umount(cancellable);
+        }
         GSystem.file_rename(tmppath, path, cancellable);
         print("Created: " + path.get_path());
     }
