727135ea8ce34bce40899a3d1366e551a5a9552d
James Antill <james@and.org>
2014-10-16 01:45:15 -0400

    treecompose: Add version option, with "minor, refresh, cve" autoincrements

--- src/py/rpmostreecompose/treecompose.py
+++ src/py/rpmostreecompose/treecompose.py
@@ -30,6 +30,21 @@ import iniparse
 from .taskbase import TaskBase
 from .utils import run_sync, fail_msg
 
+def _rev2version(repo, rev):
+    worked,oldrev = repo.resolve_rev(rev, True)
+    if not worked:
+        return None
+
+    worked,coldrev = repo.load_variant(OSTree.ObjectType.COMMIT, oldrev)
+    if not worked:
+        return None
+
+    metadata = coldrev.get_child_value(0)
+    version = metadata.lookup_value("version", None)
+    if version is not None:
+        version = version.get_string()
+    return version
+
 class Treecompose(TaskBase):
     def compose_tree(self):
         # XXX: rpm-ostree should be handling this, I think
@@ -38,6 +53,62 @@ class Treecompose(TaskBase):
             self.tree_file = '%s/%s-%s.json' % (self.pkgdatadir, self.os_name,
                                                 self.tree_name)
         rpmostreecmd = ['rpm-ostree', 'compose', 'tree', '--repo=' + self.ostree_repo]
+
+        loaded_version = _rev2version(self.repo, self.ref)
+
+        # Load the old version from the tree...
+        if loaded_version and not self.tree_version:
+            print >>sys.stderr, " WARNING: No version specified, but have old version in tree."
+
+        try:
+            lv = [int(x) for x in loaded_version.split('.', 3)]
+        except:
+            print >>sys.stderr, " WARNING: Old version is invalid (not 4 numbers)."
+            loaded_version = None
+
+        if self.tree_version:
+
+            # Version looks like <releasever>.<minor>.<refresh>.<cve>
+            # So if we are on 1.2.4.8 then:
+            # --version=minor   == 1.3.0.0
+            # --version=refresh == 1.2.5.0
+            # --version=cve     == 1.2.4.9
+            if not loaded_version and self.tree_version in ('cve', 'minor',
+                                                            'refresh'):
+                fail_msg("No previous version to get new version from")
+            if loaded_version and self.tree_version == 'cve':
+                lv = [int(x) for x in loaded_version.split('.')]
+                lv[3] += 1
+                self.tree_version = "%u.%u.%u.%u" % tuple(lv)
+            if loaded_version and self.tree_version == 'refresh':
+                lv = [int(x) for x in loaded_version.split('.')]
+                lv[2] += 1
+                lv[3] = 0
+                self.tree_version = "%u.%u.%u.%u" % tuple(lv)
+            if loaded_version and self.tree_version == 'minor':
+                lv = [int(x) for x in loaded_version.split('.')]
+                lv[1] += 1
+                lv[2] = 0
+                lv[3] = 0
+                self.tree_version = "%u.%u.%u.%u" % tuple(lv)
+
+            tv = self.tree_version.split('.')
+            if len(tv) != 4:
+                fail_msg("Version not in correct format (4 numbers). Eg. version=1.2.3.4")
+
+            if loaded_version:
+                lv = [int(x) for x in loaded_version.split('.')]
+
+                if int(tv[0]) < lv[0]:
+                    fail_msg("<releasever> of version is getting older.")
+                if int(tv[1]) < lv[1]:
+                    fail_msg("<minor> of version is getting older.")
+                if int(tv[2]) < lv[2]:
+                    fail_msg("<refresh> of version is getting older.")
+                if int(tv[3]) < lv[3]:
+                    fail_msg("<cve> of version is getting older.")
+            print "** Building Version:", self.tree_version
+            rpmostreecmd.append('--add-metadata-string=version=' + self.tree_version)
 
         rpmostreecachedir = self.rpmostree_cache_dir
         if rpmostreecachedir is not None:
@@ -57,10 +128,12 @@ def main():
     parser = argparse.ArgumentParser(description='Compose OSTree tree')
     parser.add_argument('-c', '--config', type=str, required=True, help='Path to config file')
     parser.add_argument('-r', '--release', type=str, default='rawhide', help='Release to compose (references a config file section)')
+    parser.add_argument('-V', '--version', type=str, default=None, help='Version to mark compose')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
     args = parser.parse_args()
 
     composer = Treecompose(args.config, release=args.release)
+    composer.tree_version = args.version
     composer.show_config()
 
     origrev, newrev = composer.compose_tree()
