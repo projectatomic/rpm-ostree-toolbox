84c4a86ff22f1064fc397461533c9da0a647bd44
Colin Walters <walters@verbum.org>
2014-10-28 18:47:38 -0400

    imagefactory: Add a comment for compat=0.10

--- src/py/rpmostreecompose/imagefactory.py
+++ src/py/rpmostreecompose/imagefactory.py
@@ -235,6 +235,7 @@ class ImageFactoryTask(TaskBase):
             outputname = os.path.join(outputdir, '%s.raw' % (self._name))
             print outputname 
 
+            # We use compat=0.10 to ensure we run on RHEL6 era QEMU
             qemucmd = ['qemu-img', 'convert', '-f', 'raw', '-O', 'qcow2', '-o', 'compat=0.10', image.data, outputname]
             imageouttypes.pop(imageouttypes.index("raw"))
             subprocess.check_call(qemucmd)
