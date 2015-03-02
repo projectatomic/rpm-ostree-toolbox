b1e80aef3e13a822683cf80a8fcc99cb16f2662c
Colin Walters <walters@verbum.org>
2015-03-01 22:12:32 -0500

    Grant all containers --privileged for easier data sharing
    This obviously kind of sucks, but currently it's required to label
    data accessed by non-privileged containers with
    `svirt_sandbox_file_t`.
    
    We could just make a temporary copy of the files and relabel or
    something, but since every other container is `--privileged=true`,
    let's just kick this can down the road a bit.
    
    Clearly doing more generic rel-eng stuff with Docker where data is
    stored on the host is going to run into this more.

--- src/py/rpmostreecompose/imagefactory.py
+++ src/py/rpmostreecompose/imagefactory.py
@@ -239,7 +239,7 @@ class AbstractImageFactoryTask(ImageTaskBase):
         os.mkdir(contextdir)
         ksworker_name = self.buildDockerWorker('kickstart', ['pykickstart'], dockerfile, contextdir)
 
-        cmd = ['docker', 'run', '--rm', '--workdir', '/out', '-it', '--net=none',
+        cmd = ['docker', 'run', '--rm', '--privileged', '--workdir', '/out', '-it', '--net=none',
                '-v', '{0}:{1}:ro'.format(os.path.dirname(ksfile), '/in'),
                '-v', '{0}:{1}'.format(contextdir, '/out'),
                ksworker_name]
